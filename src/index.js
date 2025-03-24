53745477150aa0e947274fd72f28194bd0dadae1
JairoApa <141659208+JairoApa@users.noreply.github.com>
2025-03-24 23:33:54 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-export default {
+// src/index.js
+var index_default = {
   async fetch(request, env) {
     const url = new URL(request.url);
     const path = url.pathname;
-
-    if (path.endsWith('.m3u8')) {
+    if (path.endsWith(".m3u8")) {
       let currentSequence = parseInt(await env.HLS_SEQUENCE.get("sequence")) || 0;
-      console.log("currentSequence inicial: ", currentSequence); // Verificación de secuencia
-
+      console.log("currentSequence inicial: ", currentSequence);
       let m3u8Text = `#EXTM3U
 #EXT-X-VERSION:3
 #EXT-X-TARGETDURATION:10
@@ -14,34 +13,32 @@ export default {
 #EXT-X-PLAYLIST-TYPE:LIVE
 #EXT-X-ALLOW-CACHE:NO
 `;
-
-      const segments = Array.from({ length: 209 }, (_, i) => `https://raw.githubusercontent.com/jairoapa/jairoapa.github.io/main/IPTV/TVStreams/stream${i}.ts`);
-
+      const segments = Array.from({ length: 209 }, (_, i) => `stream${i}.ts`);
       for (let i = 0; i < 5; i++) {
         const segmentIndex = (currentSequence + i) % segments.length;
-        const timestamp = Date.now(); // Evitar caché
+        const timestamp = Date.now();
         m3u8Text += `#EXTINF:10.0,
 https://raw.githubusercontent.com/jairoapa/jairoapa.github.io/main/IPTV/TVStreams/${segments[segmentIndex]}?t=${timestamp}
 `;
-        console.log("Segmento añadido:", segments[segmentIndex]); // Verificación de segmentos
+        console.log("Segmento a\xF1adido:", segments[segmentIndex]);
       }
-
-      // Incrementar y guardar la secuencia en KV Storage
       currentSequence = (currentSequence + 1) % segments.length;
       await env.HLS_SEQUENCE.put("sequence", currentSequence.toString());
-      console.log("Secuencia actualizada:", currentSequence); // Verificación de secuencia después de incremento
-
+      console.log("Secuencia actualizada:", currentSequence);
       return new Response(m3u8Text, {
         headers: {
           "Content-Type": "application/vnd.apple.mpegurl",
           "Access-Control-Allow-Origin": "*",
           "Cache-Control": "no-store, no-cache, must-revalidate, max-age=0",
           "Expires": "0",
-          "Pragma": "no-cache",
-        },
+          "Pragma": "no-cache"
+        }
       });
     }
-
     return new Response("Archivo no encontrado", { status: 404 });
   }
 };
+export {
+  index_default as default
+};
+//# sourceMappingURL=index.js.map
